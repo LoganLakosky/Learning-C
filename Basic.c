ec3b7b683c988c03921bbb535408cd8ecdff9226
Logan <59356357+LoganLakosky@users.noreply.github.com>
2023-09-27 11:54:20 -0500

    taking user input

--- Basic.c
+++ Basic.c
@@ -1,22 +1,84 @@
 #include <stdio.h>
-
-int main(){
-
-  int x;
-  x = 123;
-  int y = 321;
-
-  int age = 10;
-  float gpa = 12.0;
-  double b = 123123;
-  char grade = 'C';
-  char name[] = "123123123";
-  char ff = 1;
-
-  unsigned int yo = -100;
+#include <math.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 
-  printf("%f", ff);
+
+int checkOperatorType(int operator) {
+  if (operator != '+' && operator != '-' && operator != '/' && operator != '*') {
+    printf("\nInvalid operator. Please try again\n");
+
+    return 0;
+  }
+
+  return 1;
+}
+
+
+char reTakeUserInput() {
+  char newOperator;
+  printf("Please enter either +, -, / or *\n");
+  scanf(" %c", &newOperator);
+  return newOperator;
+}
+
+int main() {
+  char operator;
+  double num1;
+  double num2;
+  double result;
+
+  printf("Enter the operator: ");
+  scanf("%c", &operator);
+  while (getchar() != '\n')
+    continue;
+
+  if (!checkOperatorType(operator)) {
+    const char tmpOperator = reTakeUserInput();
+
+    if (!checkOperatorType(tmpOperator)) {
+      printf("Stop breaking my program please.");
+      return 0;
+    }
+
+    operator = tmpOperator;
+
+  }
+
+
+
+  printf("Enter the first number: ");
+  scanf("%lf", &num1);
+
+  printf("Enter the second number: ");
+  scanf("%lf", &num2);
+
+  switch (operator) {
+  case '+': {
+    result = num1 + num2;
+    printf("The result is: %.1lf", result);
+    break;
+  } case '-': {
+    result = num1 - num2;
+    printf("The result is: %.1lf", result);
+    break;
+  }
+  case '/': {
+    result = num1 / num2;
+    printf("The result is %.1lf", result);
+    break;
+  }
+  case '*': {
+    result = num1 * num2;
+    printf("The result is %lf", result);
+    break;
+  }
+
+  default: {
+    printf("The operator you have entered is invalid please try again");
+  }
+  }
 
 
   return 0;
