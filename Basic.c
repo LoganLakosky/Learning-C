fd374bf683985f1de046326c01d262331182a4d7
Logan <59356357+LoganLakosky@users.noreply.github.com>
2023-09-27 12:15:35 -0500

    Repeat message

--- Basic.c
+++ Basic.c
@@ -5,79 +5,31 @@
 
 
 
-int checkOperatorType(int operator) {
-  if (operator != '+' && operator != '-' && operator != '/' && operator != '*') {
-    printf("\nInvalid operator. Please try again\n");
-
-    return 0;
-  }
-
-  return 1;
+void printStr(char stringToRepeat[25]) {
+  printf("\n%s", stringToRepeat);
 }
 
 
-char reTakeUserInput() {
-  char newOperator;
-  printf("Please enter either +, -, / or *\n");
-  scanf(" %c", &newOperator);
-  return newOperator;
-}
 
 int main() {
-  char operator;
-  double num1;
-  double num2;
-  double result;
 
-  printf("Enter the operator: ");
-  scanf("%c", &operator);
-  while (getchar() != '\n')
-    continue;
 
-  if (!checkOperatorType(operator)) {
-    const char tmpOperator = reTakeUserInput();
+  char stringToRepeat[25];
+  printf("What message would you like to repeat? ");
+  fgets(stringToRepeat, 25, stdin);
 
-    if (!checkOperatorType(tmpOperator)) {
-      printf("Stop breaking my program please.");
-      return 0;
-    }
-
-    operator = tmpOperator;
-
-  }
+  int numberOfTimesToRepeat;
+  printf("How many times would you like to repeat your message ");
+  scanf("%d", &numberOfTimesToRepeat);
 
 
 
-  printf("Enter the first number: ");
-  scanf("%lf", &num1);
 
-  printf("Enter the second number: ");
-  scanf("%lf", &num2);
 
-  switch (operator) {
-  case '+': {
-    result = num1 + num2;
-    printf("The result is: %.1lf", result);
-    break;
-  } case '-': {
-    result = num1 - num2;
-    printf("The result is: %.1lf", result);
-    break;
-  }
-  case '/': {
-    result = num1 / num2;
-    printf("The result is %.1lf", result);
-    break;
-  }
-  case '*': {
-    result = num1 * num2;
-    printf("The result is %lf", result);
-    break;
-  }
-
-  default: {
-    printf("The operator you have entered is invalid please try again");
-  }
+  int i = 0;
+  while (i < numberOfTimesToRepeat) {
+    printStr(stringToRepeat);
+    i += 1;
   }
 
 
