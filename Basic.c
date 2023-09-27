03c507f3319a8b8be0db4fd5a783a5c0a3f75dd6
Logan <59356357+LoganLakosky@users.noreply.github.com>
2023-09-27 09:23:21 -0500

    Learning C day 1

---
+++ Basic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+  int x;
+  x = 123;
+  int y = 321;
+
+  int age = 10;
+  float gpa = 12.0;
+  double b = 123123;
+  char grade = 'C';
+  char name[] = "123123123";
+  char ff = 1;
+
+  unsigned int yo = -100;
+
+
+  printf("%f", ff);
+
+
+  return 0;
+}
