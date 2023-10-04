fd9624180154bd49451812a5f92fa495491b7b13
Logan <59356357+LoganLakosky@users.noreply.github.com>
2023-10-04 11:21:22 -0500

    Linked list in c

--- Basic.c
+++ Basic.c
@@ -1,59 +1,165 @@
 #include <stdio.h>
-#include <math.h>
-#include <ctype.h>
-#include <stdbool.h>
-#include <string.h>
 #include <stdlib.h>
-#include <time.h>
+
+typedef struct {
+  void* next;
+  int data;
+} Node;
 
 
-int addition(int num1, int num2) {
-  return num1 + num2;
+Node* head = NULL;
+
+Node* addNode(int data) {
+
+  Node* new = NULL;
+
+  if (head == NULL) {
+    new = malloc(sizeof(Node));
+    if (new == NULL) {
+      return NULL;
+    }
+
+    new->data = data;
+    head = new;
+    new->next = NULL;
+  }
+  else {
+    new = malloc(sizeof(Node));
+    if (new == NULL) {
+      return NULL;
+    }
+
+    new->data = data;
+    new->next = head;
+    head = new;
+  }
+
+  return new;
 }
 
-int subtraction(int num1, int num2) {
-  return num1 - num2;
+
+Node* insertNode(int data, int pos) {
+  Node* current = head;
+  while (current != NULL && pos != 0) {
+    pos--;
+
+  }
+
+  if (pos != 0) {
+    printf("Requested position to far into list");
+    return NULL;
+  }
+
+  Node* new = malloc(sizeof(Node));
+  if (new == NULL) {
+    return NULL;
+  }
+
+  new->data = data;
+  new->next = current->next;
+  current->next = new;
+
+  return new;
+
+}
+
+int removeNode(int data) {
+
+  Node* current = head;
+  Node* prev = head;
+
+  while (current != NULL) {
+    if (current->data == data) {
+      if (current == head) {
+        head = current->next;
+        free(current);
+        current = NULL;
+      }
+      else {
+        prev->next = current->next;
+        free(current);
+        current = NULL;
+      }
+
+
+      return 1;
+    }
+
+    prev = current;
+    current = current->next;
+  }
+
+  return 0;
+
 }
 
 
-int multiplication(int num1, int num2) {
-  return num1 * num2;
+
+void printList() {
+  Node* current = head;
+  while (current != NULL) {
+    printf("%d->", current->data);
+    current = current->next;
+
+  }
+  printf("\n");
+  return;
 }
 
-float division(float num1, float num2) {
-  return num1 / num2;
+
+void printMenu() {
+  printf("You have the following options:\n");
+  printf("\t1. Add a node to the list\n");
+  printf("\t2. Remove a node from the list\n");
+  printf("\t3. Insert a node into the list\n");
+  printf("\t4. Print the list\n");
+  printf("\t5. Quit.\n");
 }
-
-
 
 int main() {
 
-  int num1;
-  int num2;
-  char operation;
+  int option = -1;
+  int arg1 = 0;
+  int arg2 = 0;
+  while (option != 5) {
+    printMenu();
+    int numReceived = scanf("%d", &option);
+    if (numReceived == 1 && option > 0 && option < 5) {
+      switch (option) {
+      case 1:
+        //Add
+        printf("What data should I insert?:\n");
+        scanf("%d", &arg1);
+        Node* new = addNode(arg1);
+        break;
+      case 2:
+        printf("What data should I remove?:\n");
+        scanf("%d", &arg1);
+        int success = removeNode(arg1);
+        if (!success) printf("Element not found!");
+        break;
+      case 3:
 
+        printf("What data should I insert?:\n");
+        scanf("%d", &arg1);
+        printf("What position:\n");
+        scanf("%d", &arg2);
+        new = insertNode(arg1, arg2);
+        if (new == NULL) {
+          return 0;
+        }
+        break;
 
-  printf("What operation do you want to perform: ");
-  scanf("%c", &operation);
+      case 4:
+        //Print the list
+        printList();
+        break;
 
-  printf("What do you want your first number to be: ");
-  scanf("%d", &num1);
+      case 5:
+        break;
+      }
+    }
 
-  printf("What do you want your second number to be: ");
-  scanf("%d", &num2);
-
-
-  if (operation == '+') {
-    printf("Your final result is: %d", addition(num1, num2));
-  }
-  else if (operation == '-') {
-    printf("Your final result is: %d", subtraction(num1, num2));
-  }
-  else if (operation == '*') {
-    printf("Your final result is: %d", multiplication(num1, num2));
-  }
-  else if (operation == '/') {
-    printf("Your final result is: %.2f", division(num1, num2));
   }
 
   return 0;
