6b857e9eddd84c58b2a420ab572008d5ccacb05a
Logan <59356357+LoganLakosky@users.noreply.github.com>
2023-10-11 11:59:29 -0500

    Tic tac toe

--- cpppractice.cpp
+++ cpppractice.cpp
@@ -13,46 +13,115 @@ using std::getline;
 
 
 
-int getDigit(const int number) {
-  return number % 10 + (number / 10 % 10);
+void drawBoard(char* spaces) {
+  cout << '\n';
+  cout << "     |     |     " << '\n';
+  cout << " " << spaces[0] << "  |  " << spaces[1] << "  |  " << spaces[2] << " " << '\n';
+  cout << "_____|_____|_____" << '\n';
+  cout << "     |     |     " << '\n';
+  cout << " " << spaces[3] << "  |  " << spaces[4] << "  |  " << spaces[5] << " " << '\n';
+  cout << "_____|_____|_____" << '\n';
+  cout << "     |     |     " << '\n';
+  cout << " " << spaces[6] << "  |  " << spaces[7] << "  |  " << spaces[8] << " " << '\n';
+  cout << "     |     |     " << '\n';
+  cout << '\n';
+
 }
 
-int sumOddDigits(const string cardNumber) {
-  int sum = 0;
+void playerMove(char* spaces, char player) {
+  int number;
+  do {
 
-  for (int i = cardNumber.size() - 1; i >= 0; i -= 2) {
-    sum += cardNumber[i] - '0';
+    cout << "Enter a spot to place a marker (1-9)";
+    cin >> number;
+    number--;
+    if (spaces[number] == ' ') {
+      spaces[number] = player;
+      break;
+    }
+
+  } while (number < 0 || number > 8);
+}
+
+void computerMove(char* spaces, char computer) {
+  int number;
+
+  srand(time(0));
+
+
+  while (true) {
+    number = rand() % 9;
+    if (spaces[number] == ' ') {
+      spaces[number] = computer;
+      break;
+    }
   }
 
-  return sum;
 }
 
-int sumEvenDigits(const string cardNumber) {
-  int sum = 0;
+bool checkWinner(char* spaces, char player, char computer) {
 
-  for (int i = cardNumber.size() - 2; i >= 0; i -= 2) {
-    sum += getDigit(cardNumber[i] - '0' * 2);
+  if (spaces[0] == spaces[1] && spaces[1] == spaces[2] && spaces[0] != ' ') {
+    spaces[0] == player ? cout << "You win!!" : cout << "You lose!";
+  }
+  else if (spaces[3] == spaces[4] && spaces[4] == spaces[5] && spaces[3] != ' ') {
+    spaces[3] == player ? cout << "You win!!" : cout << "You lose!";
+  }
+  else if (spaces[6] == spaces[7] && spaces[7] == spaces[8] && spaces[6] != ' ') {
+    spaces[6] == player ? cout << "You win!!" : cout << "You lose!";
+  }
+  else if (spaces[0] == spaces[4] && spaces[4] == spaces[9] && spaces[0] != ' ') {
+    spaces[0] == player ? cout << "You win!!" : cout << "You lose!";
+  }
+  else if (spaces[2] == spaces[4] && spaces[4] == spaces[6] && spaces[2] != ' ') {
+    spaces[2] == player ? cout << "You win!!" : cout << "You lose!";
   }
 
-  return sum;
+
+  return 0;
+
+
+
 }
+
+bool checkTie(char* spaces) {
+  return 0;
+}
+
 
 
 int main() {
 
-  string cardNumber;
-  int result = 0;
+  char spaces[9] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+  char player = 'X';
+  char computer = 'O';
+  bool running = true;
 
-  cout << "Enter a credit card #: ";
-  cin >> cardNumber;
+  drawBoard(spaces);
 
-  result = sumEvenDigits(cardNumber) + sumOddDigits(cardNumber);
+  while (running) {
+    playerMove(spaces, player);
+    drawBoard(spaces);
 
-  if (result % 10 == 0) {
-    cout << cardNumber << " is valid";
-  }
-  else {
-    cout << cardNumber << " is not valid";
+    if (checkWinner(spaces, player, computer)) {
+      running = false;
+      break;
+    }
+    else if (checkTie(spaces)) {
+      running = false;
+      break;
+    }
+
+    computerMove(spaces, computer);
+    drawBoard(spaces);
+    if (checkWinner(spaces, player, computer)) {
+      running = false;
+      break;
+    } else if (checkTie(spaces)) {
+      running = false;
+      break;
+    }
+
   }
 
 
